bab883d7c8f560bf8a8357703453ac822a8f00c0
sjkhwaja <103473781+sjkhwaja@users.noreply.github.com>
2022-10-28 22:42:54 -0400

    update

---
+++ sql_table_creation.py
@@ -0,0 +1,19 @@
+### import packages
+import dbm
+import pandas as pd 
+import sqlalchemy
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+AZURE_MYSQL_HOSTNAME = os.getenv("AZURE_MYSQL_HOSTNAME")
+AZURE_MYSQL_USER = os.getenv("AZURE_MYSQL_USERNAME")
+AZURE_MYSQL_PASSWORD = os.getenv("AZURE_MYSQL_PASSWORD")
+AZURE_MYSQL_DATABASE = os.getenv("AZURE_MYSQL_DATABASE")
+
+connection_string_azure = f'mysql+pymysql://{AZURE_MYSQL_USER}:{AZURE_MYSQL_PASSWORD}@{AZURE_MYSQL_HOSTNAME}:3306/{AZURE_MYSQL_DATABASE}'
+db_azure = create_engine(connection_string_azure)
+
+### show dbs
+tablenames_azure = db_azure.table_names()
