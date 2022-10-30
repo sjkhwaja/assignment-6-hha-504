0f193b3df0d72d4bc5187f30bd1d2ac7f4539a7a
sjkhwaja <103473781+sjkhwaja@users.noreply.github.com>
2022-10-29 20:56:06 -0400

    final

--- sql_table_creation.py
+++ sql_table_creation.py
@@ -16,4 +16,90 @@ connection_string_azure = f'mysql+pymysql://{AZURE_MYSQL_USER}:{AZURE_MYSQL_PASS
 db_azure = create_engine(connection_string_azure)
 
 ### show dbs
-tablenames_azure = db_azure.table_names()
+db_azure.table_names() 
+print(db_azure.table_names())
+
+### creating tables  
+production_patients = """
+create table if not exists production_patients (
+    id int auto_increment,
+    mrn varchar(255) default null unique,
+    first_name varchar(255) default null,
+    last_name varchar(255) default null,
+    zip_code varchar(255) default null,
+    dob varchar(255) default null,
+    gender varchar(255) default null,
+    contact_mobile varchar(255) default null,
+    contact_home varchar(255) default null,
+    PRIMARY KEY (id) 
+); 
+"""
+
+medications = """
+create table if not exists medications (
+    id int auto_increment,
+    med_ndc varchar(255) default null unique,
+    med_human_name varchar(255) default null,
+    med_is_dangerous varchar(255) default null,
+    PRIMARY KEY (id)
+); 
+"""
+
+treatment_procedures = """
+create table if not exists treatment_procedures (
+    id int auto_increment,
+    cpt_code varchar(255) default null unique,
+    cpt_description varchar(255) default null,
+    PRIMARY KEY (id) 
+); 
+"""
+
+conditions = """
+create table if not exists production_conditions (
+    id int auto_increment,
+    icd10_code varchar(255) default null unique,
+    icd10_description varchar(255) default null,
+    PRIMARY KEY (id) 
+); 
+"""
+
+patient_meds = """
+create table if not exists patient_medication (
+    id int auto_increment,
+    mrn varchar(255) default null,
+    med_ndc varchar(255) default null,
+    PRIMARY KEY (id),
+    FOREIGN KEY (mrn) REFERENCES production_patients(mrn) ON DELETE CASCADE,
+    FOREIGN KEY (med_ndc) REFERENCES medications(med_ndc) ON DELETE CASCADE
+); 
+"""
+
+patient_treatment_procedures = """
+create table if not exists patient_treatment_procedures (
+    id int auto_increment,
+    mrn varchar(255) default null,
+    cpt_code varchar(255) default null,
+    PRIMARY KEY (id),
+    FOREIGN KEY (mrn) REFERENCES production_patients(mrn) ON DELETE CASCADE,
+    FOREIGN KEY (cpt_code) REFERENCES treatment_procedures(cpt_code) ON DELETE CASCADE
+); 
+"""
+
+patient_conditions = """
+create table if not exists patient_conditions (
+    id int auto_increment,
+    mrn varchar(255) default null,
+    icd10_code varchar(255) default null,
+    PRIMARY KEY (id),
+    FOREIGN KEY (mrn) REFERENCES production_patients(mrn) ON DELETE CASCADE,
+    FOREIGN KEY (icd10_code) REFERENCES conditions(icd10_code) ON DELETE CASCADE
+); 
+"""
+
+db_azure.execute(production_patients)
+db_azure.execute(medications)
+db_azure.execute(treatment_procedures)
+db_azure.execute(conditions)
+db_azure.execute(patient_meds)
+db_azure.execute(patient_treatment_procedures)
+db_azure.execute(patient_conditions)
